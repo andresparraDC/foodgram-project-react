85db3afa029f6ab89dd1ee52b8e951e0593f24cc
AndresParra <freddyandresparraorellana@yandex.com>
2023-01-13 21:32:42 +0300

    Requirements.txt fixed for Yandex Cloud

--- backend/foodgram/load_csv.py
+++ backend/foodgram/load_csv.py
@@ -9,14 +9,14 @@ import csv
 import os
 import django
 
-from foodgram_app.models import Ingredient
-
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "foodgram.settings"
 )
 django.setup()
+
+
+from foodgram_app.models import Ingredient
 
 
 with open('ingredients.csv', encoding="utf-8") as f:
